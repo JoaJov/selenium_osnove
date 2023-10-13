b14c7b1fa0be11edacc4e70173616a4ec87addcb
JoaJov <jovanajovanovic493@gmail.com>
2023-10-13 16:30:40 +0100

    Verify User name Is Missing

---
+++ src/main/java/Retry/SwagLabsRetry.java
@@ -0,0 +1,20 @@
+package Retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class SwagLabsRetry implements IRetryAnalyzer {
+
+
+    private int count = 0;
+    private int maxRetry = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
